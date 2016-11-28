88b7dae575112d3ec591a8527e5afc2746ee73c2
fritzOSU03 <fritzOSU03@hotmail.com>
2016-11-28 16:48:47 -0600

    Simplified and converted.

--- Working_Folder/TimerFunctions.asm
+++ Working_Folder/TimerFunctions.asm
@@ -1,165 +1,129 @@
 .data
-	newLine:		.asciiz "\n"
-	timer:			.word 60
+	timer:		.word	0
+	startTime:	.word	0
+	currentTime:	.word	0
 	
-	timeLeft:		.asciiz "Time left: "
-	timeOut:		.asciiz "You're out of time!"
-	
-	prompt:			.asciiz "Enter 1 or 0: "
-
 .text
-	lw $s6, timer						# Setup initial timer length
-	
-	li $v0, 4				
-	la $a0, timeLeft
-	syscall
-	
-	li $v0, 1
-	lw $a0, timer						# Display time left
-	syscall
-	
-	li $v0, 4
-	la $a0, newLine
-	syscall
-	
-	jal setInitialTime
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#	The test loops are just to test that my timer algorithm works	       #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#	
-testLoop:
-	li $v0, 4
-	la $a0, prompt						# Asking user for an input
-	syscall							# Input anything above '1' to end the program
-	
-	li $v0, 5
-	syscall
-	move $t6, $v0
-	
-	jal setCurrentTime
-								# Check the user's input
-	beq $t6, 1, testLoop.Increment				# If '1' we'll treat it as they got a right answer, hence increment
-	beqz $t6, testLoop.Wrong				# If '0' we treat as if the got an incorrect word
-	bgt $t6, 1, testLoop.End				# This only exists for purpose of testing to exit the program quickly.
-	
-testLoop.Increment:
-	jal incrementTime
-	
-	li $v0, 4
-	la $a0, timeLeft
-	syscall
-	
-	li $v0, 1
-	lw $a0, timer						# Show how much time is left
-	syscall
-	
-	li $v0, 4
-	la $a0, newLine
-	syscall
-	
-	jal setInitialTime					# Update the initial time 
-	
-	j testLoop
-
-testLoop.Wrong:
-	jal updateTime
-	
-	li $v0, 4
-	la $a0, timeLeft
-	syscall
-	
-	li $v0, 1
-	lw $a0, timer						# Show how much time is left
-	syscall
-	
-	li $v0, 4
-	la $a0, newLine
-	syscall
-	
-	jal setInitialTime					# Update the initial time
-	
-	j testLoop
-
-testLoop.End:
-	li $v0, 10
-	syscall
 
 #
 #	This procedure is used to set up the first time read
 #	i.e. one would use this at the beginning of the game
 #	and any time after the updated time has been displayed
 #
-
 setInitialTime:
-	li $v0, 30						# This reads in the current time from the CPU clock
-	syscall
-								# Time is stored into $a0, and $a1
-	move $t0, $a0						# So we move these values into temporaries
-	move $t1, $a1
+	addi	$sp, $sp, -16				# Move the stack pointer to allocate space
+	sw	$s1, 12($sp)				# Store $s1 on the stack
+	sw	$a1, 8($sp)				# Store $a1 on the stack
+	sw	$a0, 4($sp)				# Store $a0 on the stack
+	sw	$ra, 0($sp)				# Store $ra on the stack
 	
-	addu $s0, $t0, $t1					# Use 64-bit addition to get the full time read from $v0, 30
+	li	$v0, 30					# This reads in the current time from the CPU clock
+	syscall						# Time is stored into $a0, and $a1
+	addu	$s0, $a0, $a1				# Use 64-bit addition to get the full time read from $v0, 30
+	sw	$s0, startTime				# Store startTime in ms
 	
-	jr $ra
+	li	$t0, 60000				# $t0 = 60000 (ms)
+	sw	$t0, timer				# timer = 60000 (ms)
+	
+	lw	$ra, 0($sp)				# Restore $ra
+	lw	$a0, 4($sp)				# Restore $a0
+	lw	$a1, 8($sp)				# Restore $a1
+	lw	$s1, 12($sp)				# Restore $s1
+	addi	$sp, $sp, 16				# Restore the stack pointer
+	
+	jr	$ra					# Return to caller
+
 
 #
 #	This procedure is used to read the time directly after
 #	the user has input a word. We need this second time so
 #	that we can calculate how much time has passed.
-
+#
 setCurrentTime:
-	li $v0, 30
-	syscall
+	addi	$sp, $sp, -16				# Move the stack pointer to allocate space
+	sw	$s0, 12($sp)				# Store $s0 on the stack
+	sw	$a1, 8($sp)				# Store $a1 on the stack
+	sw	$a0, 4($sp)				# Store $a0 on the stack
+	sw	$ra, 0($sp)				# Store $ra on the stack
 	
-	move $t0, $a0						# Again like before store the time into temporaries
-	move $t1, $a1
+	li	$v0, 30					# This reads in the current time from the CPU clock
+	syscall						# Time is stored into $a0, and $a1
+	addu	$s0, $a0, $a1				# Add them together with 64-bit addition
+	sw	$s0, currentTime			# Store currentTime in ms
 	
-	addu $s1, $t0, $t1					# Add them together with 64-bit addition
+	lw	$ra, 0($sp)				# Restore $ra
+	lw	$a0, 4($sp)				# Restore $a0
+	lw	$a1, 8($sp)				# Restore $a1
+	lw	$s1, 12($sp)				# Restore $s1
+	addi	$sp, $sp, 16				# Restore the stack pointer
 	
-	jr $ra
+	jr	$ra
 
 #
 #	This procedure calculates how much time has passed since
-#	our initial time reading, then updates the timer.
+#	our initial time reading, then updates the timer. Time remaining
+#	in milliseconds is returned.
 #
+getTimeRemaining:
+	addi	$sp, $sp, -20				# Move the stack pointer to allocate space
+	sw	$s6, 16($sp)				# Store $s6 on the stack
+	sw	$s2, 12($sp)				# Store $s2 on the stack
+	sw	$s1, 8($sp)				# Store $s1 on the stack
+	sw	$s0, 4($sp)				# Store $s0 on the stack
+	sw	$ra, 0($sp)				# Store $ra on the stack
+	
+	jal	setCurrentTime				# Update the currentTime value
+	lw	$s6, timer				# $s6 = (timer)
+	lw	$s1, currentTime			# $s1 = (currentTime)
+	lw	$s0, startTime				# $s0 = (startTime)
+	
+	subu	$s2, $s1, $s0				# Subtracting the initial time from the current time
+	
+	sub	$s6, $s6, $s2				# Subtract how much time has passed from the original timer
+	sw	$s6, timer				# Update the timer variable
+	add	$v0, $zero, $s6				# $v0 = time remaining in ms
+	
+	lw	$ra, 0($sp)				# Restore $ra
+	lw	$s0, 4($sp)				# Restore $s0
+	lw	$s1, 8($sp)				# Restore $s1
+	lw	$s2, 12($sp)				# Restore $s2
+	lw	$s6, 16($sp)				# Restore $s6
+	addi	$sp, $sp, 20				# Restore the stack pointer
+	
+	jr	$ra
 
-updateTime:
-	subu $s2, $s1, $s0					# Subtracting the initial time from the current time
-	divu $s2, $s2, 1000					# Converting from miliseconds to seconds
-	
-	sub $s6, $s6, $s2					# Subtract how much time has passed from the original timer
-	sw $s6, timer						# Update the timer variable
-	
-	blt $s6, $zero, outOfTime				# Check to see if timer is less than 0, if so end the program
-	
-	jr $ra
 
 #
-#	This procedure is used for when the user inputs a correct 
-#	word for the game.
+#	This procedure is used for when the user inputs a correct
+#	word for the game. Time remaining in milliseconds is returned.
 #
+incrementTimer:
+	addi	$sp, $sp, -20				# Move the stack pointer to allocate space
+	sw	$s6, 16($sp)				# Store $s6 on the stack
+	sw	$s2, 12($sp)				# Store $s2 on the stack
+	sw	$s1, 8($sp)				# Store $s1 on the stack
+	sw	$s0, 4($sp)				# Store $s0 on the stack
+	sw	$ra, 0($sp)				# Store $ra on the stack
+	
+	jal	setCurrentTime				# Update the currentTime value
+	lw	$s0, startTime				# $s0 = (startTime)
+	lw	$s1, currentTime			# $s1 = (currentTime)
+	lw	$s6, timer				# $s6 = (timer)
+	
+	subu	$s2, $s1, $s0				# Subtract initial time from current time
+	sub	$s6, $s6, $s2				# Subtract how much time has passed from the original timer
+	blez	$s6, incrementTimer.NoTime
+	addi	$s6, $s6, 20000				# Add 20 seconds to the timer
+incrementTimer.NoTime:
+	sw	$s6, timer				# Update the timer variable
+	sub	$v0, $s6, $s2				# $v0 = timer - elapsed time = time remaining
 
-incrementTime:
-	subu $s2, $s1, $s0					# Subtract initial time from current time
-	divu $s2, $s2, 1000					# Convert from miliseconds to seconds
+incrementTimer.End:
+	lw	$ra, 0($sp)				# Restore $ra
+	lw	$s0, 4($sp)				# Restore $s0
+	lw	$s1, 8($sp)				# Restore $s1
+	lw	$s2, 12($sp)				# Restore $s2
+	lw	$s6, 16($sp)				# Restore $s6
+	addi	$sp, $sp, 20				# Restore the stack pointer
 	
-	sub $s6, $s6, $s2					# Subtract how much time has passed from the original timer
-	blt $s6, $zero, outOfTime				# Check that the timer hasn't gone below 0
-	
-	addi $s6, $s6, 20					# Add 20 seconds to the timer
-	sw $s6, timer						# Update the timer variable
-	
-	jr $ra
-
-#
-#	This procedure is used for when the player has ran out of
-#	time. It will end the program, but we can change this if 
-# 	we decide to have the game be loop based.
-#
-
-outOfTime:
-	li $v0, 4
-	la $a0, timeOut
-	syscall
-	
-	li $v0, 10
-	syscall
+	jr	$ra					# Return to caller
