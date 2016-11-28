d6b54daa912e2fd87fdce55279bd08c6e5870ea6
fritzOSU03 <fritzOSU03@hotmail.com>
2016-11-28 16:47:52 -0600

    Added functions for checking results and array copying.

--- Working_Folder/WordListFunctions.asm
+++ Working_Folder/WordListFunctions.asm
@@ -17,7 +17,13 @@
 	dictionaryWords:	.word	0
 	rdmWrdOfst:		.word	0
 	
+	badEntryWordDict:	.asciiz	"This word is not in the list of correct answers. Please try again.\n"
+	badEntryWordDupl:	.asciiz	"You've already entered this word. Please try again.\n"
+	goodEntryWord:		.asciiz	"Correct word entry!\n"
+	
 .text
+	.include "TimerFunctions.asm"
+	.include "ScoringFunctions.asm"
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #				  SetupWordFunctions Procedure						#
@@ -135,6 +141,8 @@ SetupValidWordList:
 	jal	setupDictionary				# Call setupDictionary procedure
 	
 SetupWordFunctions.End:
+	jal	setInitialTime				# Setup and Start the timer
+	jal	resetScore				# Reset the score to zero
 	lw	$ra, 0($sp)				# Restore $ra
 	lw	$a0, 4($sp)				# Restore $a0
 	lw	$a1, 8($sp)				# Restore $a1
@@ -147,7 +155,92 @@ SetupWordFunctions.End:
 	jr	$ra					# Set PC = $ra
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#					checkWord Procedure						#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#	This procedure takes the address of the enteredWord in $a0. The word is checked for presence in	#
+#	the dictionaryMatches array and it is then checked to ensure that is has not been entered	#
+#	via the givenMatches array. Once these conditions are met, a successful entry is then writen to	#
+#	the enteredWord array and the score is incremented by the length of the word.			#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+checkWord:
+	addi	$sp, $sp, -16				# Move the stack pointer to allocate space
+	sw	$s0, 12($sp)				# Store $s0 on the stack
+	sw	$a1, 8($sp)				# Store $a1 on the stack
+	sw	$a0, 4($sp)				# Store $a0 on the stack
+	sw	$ra, 0($sp)				# Store $ra on the stack
 	
+	jal	wordToLowerCase				# Call wordToLowerCase procedure
+	add	$s0, $zero, $a0				# $s0 = Address of enteredWord
+	
+	# Check for word in dictionaryMatches
+	la	$a1, dictionaryMatches			# $a1 = Address of dictionaryMatches
+	jal	strContains				# Call strContains function
+	
+	# If Not in dictionaryMatches
+	beq	$v0, 1, checkWord.InDictionary		# If($v0 == 1) branch to checkWord.InDictionary
+	
+	#	Show invalid entry message
+	li	$v0, 4					# Load syscall print string function
+	la	$a0, badEntryWordDict			# Load address of string to print
+	syscall						# Execute syscall
+	
+	#	Go to Finish
+	j	checkWord.End				# Jump to checkWord.End
+	
+checkWord.InDictionary:
+	# Check for word not in givenMatches
+	la	$a1, givenMatches			# $a1 = Address of givenMatches
+	jal	strContains				# Call strContains function
+	
+	# Else If not in givenMatches
+	beq	$v0, 0, checkWord.NotInEntries		# If($v0 == 0) branch to checkWord.NotInEntries
+	
+	#	Show duplicate entry message
+	li	$v0, 4					# Load syscall print string function
+	la	$a0, badEntryWordDupl			# Load address of string to print
+	syscall						# Execute syscall
+	
+	#	Go to Finish
+	j	checkWord.End				# Jump to checkWord.End
+	
+	# Else
+checkWord.NotInEntries:
+	#	add word to givenmatches
+	la	$a0, givenMatches			# $a0 = Address of givenMatches
+	jal	strLenNull				# Call strLenNull function
+	add	$a0, $a0, $v0				# Point to end of givenMatches
+	add	$a1, $s0, $zero				# $a1 = Address of entered word
+	jal	strCpyNewLine				# Call strCpyNewLine
+	
+	#	increment timer
+	jal	incrementTimer				# Call incrementTimer
+	
+	#	increment score
+	add	$a0, $s0, $zero				# $a0 = word string length
+	jal	incrementScore				# Call incrementScore
+	
+	#	Show valid entry message
+	li	$v0, 4					# Load syscall print string function
+	la	$a0, goodEntryWord			# Load address of string to print
+	syscall						# Execute syscall
+	
+	#	Go to Finish
+	j	checkWord.End				# Jump to checkWord.End
+	
+	
+checkWord.End:
+	lw	$ra, 0($sp)				# Restore $ra
+	lw	$a0, 4($sp)				# Restore $a0
+	lw	$a1, 8($sp)				# Restore $a1
+	lw	$s0, 12($sp)				# Restore $s0
+	addi	$sp, $sp, 16				# Restore the stack pointer
+	
+	jr	$ra					# Set PC = $ra
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #					strCpy Procedure						#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -211,11 +304,70 @@ strCpyNewLine.Loop:
 strCpyNewLine.End:
 	li	$t2, 10					# $t2 = "\n"
 	sb	$t2, 0($t3)				# Destination[i] = 10 (new line character)
+	sb	$zero, 1($t3)				# Destination[i+1] = 0 (null character)
 	
 	lw	$ra, 0($sp)				# Restore $ra
 	lw	$a0, 4($sp)				# Restore $a0
 	lw	$a1, 8($sp)				# Restore $a1
 	addi	$sp, $sp, 12				# Restore the stack pointer
+	
+	jr	$ra					# Set PC = $ra
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#					strContains Procedure						#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#	This procedure takes two addresses, the string address in $a0, and the string array (new line	#
+#	delimited) address in $a1. The string is checked for presence in the string array as a sub-	#
+#	string. A 1 is returned if the string array contains the string. Othersize, 0 is returned.	#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+strContains:
+	addi	$sp, $sp, -16				# Move the stack pointer to allocate space
+	sw	$s0, 12($sp)				# Store $s0 on the stack
+	sw	$a1, 8($sp)				# Store $a1 on the stack
+	sw	$a0, 4($sp)				# Store $a0 on the stack
+	sw	$ra, 0($sp)				# Store $ra on the stack
+	
+	add	$s0, $zero, $a0				# $s0 = Address of string (original)
+	add	$t0, $zero, $a0				# $t0 = Address of string
+	add	$t1, $zero, $a1				# $t1 = Address of string array
+	
+strContains.Loop.Start:
+	lb	$t2, 0($t0)				# $t2 = 0(string[i])
+	beq	$t2, 10, strContains.True.Conf		# If(string[i]==10) branch to strContains.True.Conf
+	beqz	$t2, strContains.True.Conf		# If(string[i]==0) branch to strContains.True.Conf
+	lb	$t3, 0($t1)				# $t3 = 0(string array[i])
+	beqz	$t3, strContains.False			# If(string array[i]==0) branch to strContains.False
+	bne	$t2, $t3, strContains.Loop.NoMatch	# If(string[i]!=string array[i]) branch to strContains.Loop.NoMatch
+strContains.Loop.Match:
+	addi	$t0, $t0, 1				# Increment string pointer
+	addi	$t1, $t1, 1				# Increment string array pointer
+	j	strContains.Loop.Start			# Jump to strContains.Loop.Start
+strContains.Loop.NoMatch:
+	add	$t0, $zero, $s0				# Reset string pointer
+strContains.Loop.ArrayNext:
+	addi	$t1, $t1, 1				# Increment string array pointer
+	lb	$t3, -1($t1)				# $t3 = -1(string array[i])
+	beq	$t3, 10, strContains.Loop.Start		# If(string array[i]==10) branch to strContains.Loop.Start
+	beqz	$t3, strContains.False			# If(string array[i]==0) branch to strContains.False
+	j	strContains.Loop.ArrayNext		# Jump to strContains.Loop.ArrayNext
+strContains.True.Conf:
+	lb	$t3, 0($t1)				# $t2 = 0(string array[i])
+	ble	$t3, 10, strContains.True		# If(string array[i]<=10) branch to strContains.True
+	j	strContains.Loop.NoMatch		# Else jump to strContains.Loop.NoMatch
+strContains.True:
+	li	$v0, 1					# $v0 = 1 (True)
+	j	strContains.End				# Jump to strContains.End
+strContains.False:
+	li	$v0, 0					# $v0 = 0 (False)
+	j	strContains.End				# Jump to strContains.End
+strContains.End:
+	lw	$ra, 0($sp)				# Restore $ra
+	lw	$a0, 4($sp)				# Restore $a0
+	lw	$a1, 8($sp)				# Restore $a1
+	lw	$s0, 12($sp)				# Restore $s0
+	addi	$sp, $sp, 16				# Restore the stack pointer
 	
 	jr	$ra					# Set PC = $ra
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -315,7 +467,7 @@ enteredWordSort.End:
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #	This procedure takes one address in $a0. The procedure checks each character to see if it falls	#
 #	within the range of lower case ascii characters (97 - 122) and adds 32 to any character outside	#
-#	of that range. The copy	conversion process completes when either a new line character (10) or	#
+#	of that range. The conversion process completes when either a new line character (10) or	#
 #	the null character (0) is found.								#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 wordToLowerCase:
@@ -537,6 +689,35 @@ strLen.Loop:
 	addi	$t2, $t2, 1				# Increment the counter
 	j	strLen.Loop				# Return to the top of the loop
 strLen.Exit:
+	add	$v0, $t2, $zero				# Move the count to $v0
+	lw	$ra, 0($sp)				# Restore $ra
+	lw	$a0, 4($sp)				# Restore $a0
+	addi	$sp, $sp, 8				# Restore the stack pointer
+	
+	jr	$ra					# Set PC = $ra
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#					strLenNull Function						#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#	This function takes the address of a string in $a0 and iterates through the string to obtain	#
+#	length of the string. The function completes only when the null terminator character (0) is	#
+#	found. The integer length of the string is returned.						#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+strLenNull:
+	addi	$sp, $sp, -8				# Move the stack pointer to allocate space
+	sw	$a0, 4($sp)				# Store $a0 on the stack
+	add	$t0, $a0, $zero				# $t0 = enteredWord
+	sw	$ra, 0($sp)				# Store $ra on the stack
+	li	$t2, 0					# Initialize the counter to zero
+strLenNull.Loop:
+	lb	$t1, 0($t0)				# Load the next character into t1
+	beqz	$t1, strLenNull.Exit			# Check for the null character
+	addi	$t0, $t0, 1				# Increment the string pointer
+	addi	$t2, $t2, 1				# Increment the counter
+	j	strLenNull.Loop				# Return to the top of the loop
+strLenNull.Exit:
 	add	$v0, $t2, $zero				# Move the count to $v0
 	lw	$ra, 0($sp)				# Restore $ra
 	lw	$a0, 4($sp)				# Restore $a0
