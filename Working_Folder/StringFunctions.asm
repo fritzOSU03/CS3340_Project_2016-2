539491fde00868edf569d3788bcb9c06a41b70c8
fritzOSU03 <fritzOSU03@hotmail.com>
2016-11-30 20:18:18 -0600

    Update StringFunctions.asm

--- Working_Folder/StringFunctions.asm
+++ Working_Folder/StringFunctions.asm
@@ -1,3 +1,6 @@
+.data
+	matrix:	.asciiz	"\t+---+---+---+\n\t| 0 | 1 | 2 |\n\t+---+---+---+\n\t| 3 | 4 | 5 |\n\t+---+---+---+\n\t| 6 | 7 | 8 |\n\t+---+---+---+\n"
+
 .text
 
 .globl	strCpy
@@ -6,6 +9,8 @@
 .globl	wordToLowerCase
 .globl	strLen
 .globl	strLenNull
+.globl	wordShuffle
+.globl	printMatrix
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -228,4 +233,130 @@ strLenNull.Exit:
 	addi	$sp, $sp, 8				# Restore the stack pointer
 	
 	jr	$ra					# Set PC = $ra
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#					wordShuffle Procedure						#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#	This procedure takes two addresses, the source address in $a1, and the destination address	#
+#	in $a0. The furst character of the source address is stored to the middle (5th) character space	#
+#	of the destination address. The remaining characters of the source address are then copied	#
+#	randomly, biy by bit, to the destination address. The copy after all nine characters have been	#
+#	copied.												#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+wordShuffle:
+	addi	$sp, $sp, -28			# Move the stack pointer to allocate space
+	sw	$s7, 24($sp)			# Store $s7 on the stack
+	sw	$s6, 20($sp)			# Store $s6 on the stack
+	sw	$s1, 16($sp)			# Store $s1 on the stack
+	sw	$s0, 12($sp)			# Store $s0 on the stack
+	sw	$a1, 8($sp)			# Store $a1 on the stack
+	sw	$a0, 4($sp)			# Store $a0 on the stack
+	sw	$ra, 0($sp)			# Store $ra on the stack
+	
+	add	$s0, $a0, $zero			# $s0 = Address of shuffledWord
+	add	$s1, $a1, $zero			# $s0 = Address of enteredWord
+	li	$t0, 1				# $t0 = 1 (counter)
+	add	$s6, $a0, $zero			# $s6 = Address of shuffledWord (backup)
+	add	$s7, $a1, $zero			# $s7 = Address of enteredWord (backup)
+	li	$t1, 9				# $t1 = 9 (max. counter)
+wordShuffle.ClearWord:
+	sb	$zero, 0($s0)			# shuffledWord = "0?????????"
+	sb	$zero, 1($s0)			# shuffledWord = "00????????"
+	sb	$zero, 2($s0)			# shuffledWord = "000???????"
+	sb	$zero, 3($s0)			# shuffledWord = "0000??????"
+	sb	$zero, 4($s0)			# shuffledWord = "00000?????"
+	sb	$zero, 5($s0)			# shuffledWord = "000000????"
+	sb	$zero, 6($s0)			# shuffledWord = "0000000???"
+	sb	$zero, 7($s0)			# shuffledWord = "00000000??"
+	sb	$zero, 8($s0)			# shuffledWord = "000000000?"
+	sb	$zero, 9($s0)			# shuffledWord = "0000000000"
+wordShuffle.WriteWord:
+	lb	$t2, 0($s1)			# $t2 = 0(enteredWord)
+	sb	$t2, 4($s0)			# 4(shuffledWord) = 0(enteredWord)
+	addi	$s1, $s1, 1			# Increment the enteredWord pointer
+	lb	$t2, 0($s1)			# Store enteredWord character to $t2
+wordShuffle.Shuffle:
+	li	$v0, 42				# Load Syscall random int range svc
+	li	$a0, 0				# From 0
+	li	$a1, 9				# To 8
+	syscall					# Execute Syscall
+	add	$t4, $a0, $zero			# Store the random int to $t4
+	add	$s0, $s0, $t4			# shuffledWord pointer += random int
+	lb	$t3, 0($s0)			# $t3 = random int(shuffledWord)
+	bnez	$t3, wordShuffle.Shuffle.NoCopy	# If($t3 != 0) branch to wordShuffle.Shuffle.NoCopy
+wordShuffle.Shuffle.Copy:			# Else, copy the character to (shuffledWord)
+	lb	$t2, 0($s1)			# Store enteredWord character to $t2
+	addi	$t0, $t0, 1			# Increment the counter
+	sb	$t2, 0($s0)			# Store enteredWord character to 0(shuffledWord)
+	addi	$s1, $s1, 1			# Increment the enteredWord pointer
+	add	$s0, $s6, $zero			# $s0 = Address of shuffledWord
+	bge	$t0, $t1, wordShuffle.End	# If(counter >= max counter) branch to wordShuffle.End
+	j	wordShuffle.Shuffle		# Jump to wordShuffle.Shuffle
+wordShuffle.Shuffle.NoCopy:
+	add	$s0, $s6, $zero			# $s0 = Address of shuffledWord
+	j	wordShuffle.Shuffle		# Jump to wordShuffle.Shuffle
+wordShuffle.End:
+	lw	$ra, 0($sp)			# Restore $ra
+	lw	$a0, 4($sp)			# Restore $a0
+	lw	$a1, 8($sp)			# Restore $a1
+	lw	$s0, 12($sp)			# Restore $s0
+	lw	$s1, 16($sp)			# Restore $s1
+	lw	$s6, 20($sp)			# Restore $s6
+	lw	$s7, 24($sp)			# Restore $s7
+	addi	$sp, $sp, 28			# Restore the stack pointer
+	
+	jr	$ra				# Return to caller
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#					printMatrix Procedure						#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#	This procedure takes one addresses, the shuffled nine-letter word address in $a0. The shuffled
+#	word is placed into a grid style matrix and the complete matrix is printed to the console.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+#\t+---+---+---+\n\t| 0 | 1 | 2 |\n\t+---+---+---+\n\t| 3 | 4 | 5 |\n\t+---+---+---+\n\t| 6 | 7 | 8 |\n\t+---+---+---+\n
+printMatrix:
+	addi	$sp, $sp, -16			# Move the stack pointer to allocate space
+	sw	$s1, 12($sp)			# Store $s1 on the stack
+	sw	$s0, 8($sp)			# Store $s0 on the stack
+	sw	$a0, 4($sp)			# Store $a0 on the stack
+	sw	$ra, 0($sp)			# Store $ra on the stack
+	
+	add	$s1, $a0, $zero			# $s1 = Address of shuffledWord
+	la	$s0, matrix			# $s0 = Address of matrix
+	lb	$t0, 0($s1)			# $t0 = 0(shuffledWord)
+	sb	$t0, 18($s0)			# 18(matrix) = 0(shuffledWord)
+	lb	$t0, 1($s1)			# $t0 = 1(shuffledWord)
+	sb	$t0, 22($s0)			# 22(matrix) = 1(shuffledWord)
+	lb	$t0, 2($s1)			# $t0 = 2(shuffledWord)
+	sb	$t0, 26($s0)			# 26(matrix) = 2(shuffledWord)
+	lb	$t0, 3($s1)			# $t0 = 3(shuffledWord)
+	sb	$t0, 48($s0)			# 48(matrix) = 3(shuffledWord)
+	lb	$t0, 4($s1)			# $t0 = 4(shuffledWord)
+	sb	$t0, 52($s0)			# 52(matrix) = 4(shuffledWord)
+	lb	$t0, 5($s1)			# $t0 = 5(shuffledWord)
+	sb	$t0, 56($s0)			# 56(matrix) = 5(shuffledWord)
+	lb	$t0, 6($s1)			# $t0 = 6(shuffledWord)
+	sb	$t0, 78($s0)			# 78(matrix) = 6(shuffledWord)
+	lb	$t0, 7($s1)			# $t0 = 7(shuffledWord)
+	sb	$t0, 82($s0)			# 82(matrix) = 7(shuffledWord)
+	lb	$t0, 8($s1)			# $t0 = 8(shuffledWord)
+	sb	$t0, 86($s0)			# 86(matrix) = 8(shuffledWord)
+	li	$v0, 4				# Load Syscall print string service
+	add	$a0, $s0, $zero			# $a0 = Address of matrix
+	syscall					# Execute Syscall
+printMatrix.End:
+	li	$v0, 0				# Clear $v0
+	
+	lw	$ra, 0($sp)			# Restore $ra
+	lw	$a0, 4($sp)			# Restore $a0
+	lw	$s0, 8($sp)			# Restore $s0
+	lw	$s1, 12($sp)			# Restore $s1
+	addi	$sp, $sp, 16			# Restore the stack pointer
+	
+	jr	$ra				# Return to caller
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
